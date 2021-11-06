094aa10f81041fcea73587cfa140ed43c435f49a
Neel Adwani <nprosofficial@gmail.com>
2021-11-06 19:26:08 +0000

    added POST route for processing the data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask('app', template_folder = "templates", static_folder = "static")
 
 @app.route('/')
@@ -7,6 +7,21 @@ def hello_world():
 
 @app.route('/try')
 def longText():
+  return render_template('tryitout.html')
+
+@app.route('/try', methods = ['POST'])
+def process():
+  title = request.form['title']
+  presenter = request.form['presenter']
+  back = request.form['back']
+  intro = request.form['intro']
+  literature = request.form['literature']
+  method = request.form['method']
+  result = request.form['result']
+  conc = request.form['conc']
+  ack = request.form['ack']
+  cite = request.form['cite']
+  end = request.form['end']
   return render_template('tryitout.html')
 
 app.run(host='0.0.0.0', port=8080)
