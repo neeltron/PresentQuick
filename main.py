d2e9196fac55d74961dbbb4042288a741ac8b153
Neel Adwani <nprosofficial@gmail.com>
2021-11-06 17:53:55 +0000

    added try page layout

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ app = Flask('app', template_folder = "templates", static_folder = "static")
 def hello_world():
   return render_template('index.html')
 
+@app.route('/try')
+def longText():
+  return render_template('tryitout.html')
+
 app.run(host='0.0.0.0', port=8080)
